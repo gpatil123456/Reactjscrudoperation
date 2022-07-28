61e754a040298b24123328bf5040eb3805a02b67
gpatil123456 <gauravpatil290198@gmail.com>
2022-07-28 08:11:22 +0530

    reactjs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,52 @@ import React, { useEffect, useState } from 'react'
 import "./app.css"
 import axios from 'axios';
 
+
 function App() {
   const [movieName, setMovieName] = useState('')
   const [review, setReview] = useState('');
-  const [movieReviewList, setMovieList] = useState ([]);
-  const [newReview, setNewReview] = useState ("");
+  const [movieReviewList, setMovieList] = useState([]);
+  const [newReview, setNewReview] = useState("");
+  const [userInfo, setuserInfo] = useState({
+    file: [],
+    filepreview: null,
+  });
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/get").then((Response) => {
+      setMovieList(Response.data);
+    });
+  }, []);
+
+  const handleInputChange = (e) => {
+
+   
+    setuserInfo({
+      ...userInfo,
+      file: e.target.files[0],
+      filepreview: URL.createObjectURL(e.target.files[0]),
+    });
+
+  }
 
 
 
   const submitReview = () => {
+    const formdata = new FormData();
+    formdata.append('avatar', userInfo.file);
+
+   
+    axios.post("http://localhost:3001/imageupload", formdata, {
+      headers: { "Content-Type": "multipart/form-data" }
+    })
+      .then(res => { 
+        console.warn(res);
+        if (res.data.success === 1) {
+          alert("Image upload successfully");
+        }
+
+      })
+
     axios.post("http://localhost:3001/api/insert", {
       movieName: movieName,
       movieReview: review,
@@ -29,19 +66,8 @@ function App() {
       movieName: movie,
       movieReview: newReview,
     });
-    setNewReview("")
+    // setNewReview("")
   };
-
-
-  useEffect(() => {
-    console.log("im here")
-    axios.get("http://localhost:3001/api/get/res")
-    .then((Response) => {
-      console.log(Response.data,  "mad")
-      setMovieList(Response.data);
-      
-    });
-  },[]);
 
 
   return (
@@ -57,30 +83,38 @@ function App() {
         <input placeholder='Enter Movie Review' onChange={(e) => {
           setReview(e.target.value)
         }} type="text" name="movieName" />
+      <input type="file" className="form-control" name="upload_file" onChange={handleInputChange} />
+        {/* <button type="submit" onClick={() => submit()} > Upload </button> */}
         <button onClick={submitReview}>Submit</button>
 
       </div>
       <div className='wrap'>
-      {movieReviewList.map((val) => {
-        return (
-          <div className='card'>
-            <h1>{val.movieName}</h1>
-            <p>{val.movieReview}</p>
-         
-            <input  placeholder='Update Movie Review' type="text" className="one" onChange={(e) => {
-              setNewReview(e.target.value)
-            }} /> 
-            <div className='button'>
-              <button onClick={() => { deleteRiview(val.movieReview) }}>Delete</button>
-              <div className='button_two'>
-            <button onClick={() => {
-              updateRiview(val.movieName)
-            }}>Update</button>
+        {movieReviewList.map((val) => {
+          return (
+            <div className='card'>
+              <h1>{val.movieName}</h1>
+              <p>{val.movieReview}</p>
+
+              <input placeholder='Update Movie Review' type="text" className="one" onChange={(e) => {
+                setNewReview(e.target.value)
+              }} />
+          
+   {userInfo.filepreview !== null ?
+                <img className="previewimg" src={userInfo.filepreview} alt="UploadImage" />
+                : null} 
+              <div className='button'>
+                <button className='dot' onClick={() => { deleteRiview(val.movieName) }}>Delete</button>
+                <div className='button_two'>
+                  <button className='dot' onClick={() => {
+                    updateRiview(val.movieName)
+                  }}>Update</button>
+                </div>
+              </div>
+            
             </div>
-            </div>
-          </div>
-        )
-      })}
+          )
+        })}
+        
       </div>
 
     </div>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
+const util = require('util');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
-const app =  express()
+const app = express()
 const mysql = require('mysql')
 const { restart } = require('nodemon')
+const multer = require('multer')
+const path = require('path');
 
 
 const db = mysql.createPool({
@@ -13,47 +16,107 @@ const db = mysql.createPool({
     database: "crud",
 });
 
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended: true}))
+db.getConnection((err, connection) => {
+    if (err) {
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.');
+      }
+      if (err.code === 'ER_CON_COUNT_ERROR') {
+        console.error('Database has too many connections.');
+      }
+      if (err.code === 'ECONNREFUSED') {
+        console.error('Database connection was refused.');
+      }
+    }
+  
+    if (connection) connection.release();
+  
+    return;
+  });
+  
+  // Promisify for Node.js async/await.
+  db.query = util.promisify(db.query);
 
-app.get("/api/get/res",(req, res)=>{
-    const sqlSelect = 
-    "SELECT *  FROM movie_reviews";
-    db.query(sqlSelect, (err, result)=>{
+  app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, '../public_html/', 'uploads'),
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+app.post('/imageupload', async (req, res) => {
+    try {
+
+        let upload = multer({ storage: storage }).single('avatar');
+
+        upload(req, res, function (err) {
+
+            if (!req.file) {
+                return res.send('Please select an image to upload');
+            }
+            else if (err instanceof multer.MulterError) {
+                return res.send(err);
+            }
+            else if (err) {
+                return res.send(err);
+            }
+
+            const classifiedsadd = {
+                image: req.file.filename
+            };
+            const sql = "INSERT INTO movie_reviews SET ?";
+            db.query(sql, classifiedsadd, (err, results) => {
+                if (err) throw err;
+                res.json({ success: 1 })
+
+            });
+
+        });
+
+    } catch (err) { console.log(err) }
+})
+
+app.get("/api/get", (req, res) => {
+    const sqlSelect =
+        "SELECT * FROM movie_reviews";
+    db.query(sqlSelect, (err, result) => {
         res.send(result)
-       console.log(result)
+        console.log(result)
     })
 
 })
 
-app.post("/api/insert", (req, res)=>{
-     const movieName = req.body.movieName;
-     const movieReview = req.body.movieReview;
+app.post("/api/insert", (req, res) => {
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
 
-     const sqlInsert = 
-     "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
-     db.query(sqlInsert, [movieName, movieReview], (err, result)=>{
+    const sqlInsert =
+        "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
         console.log(result)
-     });
+    });
 });
 
-app.delete('/api/delete/:movieReview', (req, res)=>{
-    const name = req.params.movieReview;
-    const sqldelete = "DELETE FROM movie_reviews WHERE movieReview = ?";
-    db.query(sqldelete, name, (err, result)=>{
+app.delete('/api/delete/:movieName', (req, res) => {
+    const name = req.params.movieName;
+    const sqldelete = "DELETE FROM movie_reviews WHERE movieName = ?";
+    db.query(sqldelete, name, (err, result) => {
         if (err) console.log(err);
-     });
+    });
 
 });
 
-app.put('/api/update', (req, res)=>{
+app.put('/api/update', (req, res) => {
     const name = req.body.movieName;
     const review = req.body.movieReview;
     const sqlUpdate = "UPDATE movie_reviews SET movieReview = ? WHERE movieName = ?";
-    db.query(sqlUpdate, [review, name], (err, result)=>{
+    db.query(sqlUpdate, [review, name], (err, result) => {
         if (err) console.log(err);
-     });
+    });
 
 });
 
@@ -61,7 +124,7 @@ app.get('/', (req, res) => {
     res.send("hello world")
 });
 
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log("running on port 3001");
 }
 );
